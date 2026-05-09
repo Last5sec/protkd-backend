35c200886de3e28e672365851b12fe3aa1cc068f
Harsh Tushid <harshtushid213@gmail.com>
2026-05-09 22:49:49 +0530

    migrate to postgresql

--- db.js
+++ db.js
@@ -2,9 +2,9 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_URL?.includes("railway")
-    ? { rejectUnauthorized: false }
-    : false,
+  ssl: process.env.DATABASE_URL?.includes("railway.internal")
+    ? false
+    : { rejectUnauthorized: false },
 });
 
 async function getDB() {
